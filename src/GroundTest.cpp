477ccd5129be94b8ab65c9317481da574562450f
maaL <87633094+maaL6@users.noreply.github.com>
2025-03-12 15:37:22 +0700

    Delete src/GroundTest.cpp
    ko can nua

--- src/GroundTest.cpp
+++
@@ -1,9 +0,0 @@
-// Cấu trúc để đại diện cho một đối tượng (player hoặc chướng ngại vật)
-/*
-struct GameObject {
-    SDL_Rect rect;
-    SDL_Color color;
-    float x_velocity;
-    float y_velocity;
-};
-*/
